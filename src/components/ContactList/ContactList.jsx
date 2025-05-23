93044201cad4f032440831a7cef58b0ce3451f95
Roki2221 <100676027+Roki2221@users.noreply.github.com>
2025-05-23 13:56:26 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,9 +1,9 @@
-import Contact from "../../Contact/Contact";
+import Contact from '../Contact/Contact';
 
 export default function ContactList({ onDelete, contactsData }) {
   return (
     <ul>
-      {contactsData.map((item) => {
+      {contactsData.map(item => {
         return (
           <li key={item.id}>
             <Contact onDelete={onDelete} contactInfo={item}></Contact>
